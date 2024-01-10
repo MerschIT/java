0242541a9624dee384ad76553f5658ec90ea4697
MerschIT <augusto360@hotmail.com>
2024-01-10 14:10:59 +0100

    Holis

---
+++ libretaAgenda/src/main/java/com/yacare/libretaagenda/LibretaAgenda.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.yacare.libretaagenda;
+
+import gui.Pantalla;
+
+/**
+ *
+ * @author augus
+ */
+public class LibretaAgenda {
+
+    public static void main(String[] args) {
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+}
---
+++ mavenproject1/src/main/java/com/yacare/mavenproject1/Mavenproject1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.yacare.mavenproject1;
+
+/**
+ *
+ * @author augus
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
