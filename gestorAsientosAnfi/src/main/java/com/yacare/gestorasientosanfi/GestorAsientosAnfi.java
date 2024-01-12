b8e337d30947303ec10da204f28805c2a845eb92
MerschIT <augusto360@hotmail.com>
2024-01-12 12:00:05 +0100

    Agrego proyecto Gestor de asientos

---
+++ gestorAsientosAnfi/src/main/java/com/yacare/gestorasientosanfi/GestorAsientosAnfi.java
@@ -0,0 +1,45 @@
+package com.yacare.gestorasientosanfi;
+
+
+public class GestorAsientosAnfi {
+
+    public static void main(String[] args) {
+        
+        /*
+          - Anfiteatro con 10 filas y cada fila tiene 10 asientos. 
+            Desarrollar sistema que permita seleccionar un asiento libre
+            y poder ponerlo como ocupado.
+        
+            1# Cargar el mapa de asientos, marcando con una L los asientos 
+               libres y con una X los ocupados.
+            
+            2# Solo 1 asiento puede ser ocupado por persona, si el asiento
+                ya se encuentra ocupado debe permitir a la persona elegir
+                otro. .
+                
+            3# Para finalizar el programa se deberá ingresas el valor
+                "99". No hay un límite de veces que el programa se puede 
+                ejecutar.
+        
+            4# Solo hay 10 filas y 10 asientos por fila. No se puede seleccionar
+                un asiento fuera de ese rango.
+        
+            5# No se puede utilizar GUI ni BD. Solo se debe usar desarrollo
+                lógico.
+                
+            6# En caso de que el quiente lo desee deberá poder ver en consola
+                la representación "grafica" de los asientos libres y ocupados.
+        
+        
+        
+        
+        
+        
+        
+        */
+        
+        
+        
+        
+    }
+}
